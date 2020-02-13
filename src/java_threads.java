a10d8665620d0bccf14374d6eea2555b7b02dd42
alechaws <astarofaz@gmail.com>
2020-02-12 19:28:03 -0700

    Threads

--- src/java_threads.java
+++ src/java_threads.java
@@ -1,50 +1,34 @@
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-
-public class java_threads {
-    public static void main(String[] args) {
-        List< Integer > deck = Arrays.asList(1, 1, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
-        int max = 51;
-        int min = 0;
-
-
-        Queue< Double > theDeck = new LinkedList< >();
-
-        for (int count = 0; count <= 25; count++) {
-            int cardNumber = ThreadLocalRandom.current().nextInt(min, max + 1);
-            int deal = deck.get(cardNumber);
-            theDeck.add((double) deal);
+class MyThread implements Runnable {
+    String name;
+    Thread t;
+    MyThread (String thread){
+        name = "My Thread";
+        t = new Thread(this, name);
+        System.out.println("New thread: " + t);
+        t.start();
+    }
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e) {
+            System.out.println(name + "Interrupted");
         }
-
-        //System.out.println(theDeck);
-        List < Double > dealerHand = new ArrayList< >();
-        double dealerDraw1 = theDeck.remove();
-        dealerHand.add(dealerDraw1);
-        double dealerDraw2 = theDeck.remove();
-        dealerHand.add(dealerDraw2);
-
-        List < Double > playerHand = new ArrayList < > ();
-        double playerDraw1 = theDeck.remove();
-        playerHand.add(playerDraw1);
-        double playerDraw2 = theDeck.remove();
-        playerHand.add(playerDraw2);
-        double playerTotal = playerHand.get(0) + playerHand.get(1);
-        double dealerTotal = dealerHand.get(0) + dealerHand.get(1);
-
-
-        System.out.println("\nThe dealer has " + dealerHand.get(0) + " and " + dealerHand.get(1));
-        System.out.println("The total in the dealer's hand is " + dealerTotal);
-        System.out.println("\nYou have " + playerHand.get(0) + " and " + playerHand.get(1));
-        System.out.println("The total of your hand is " + playerTotal);
-
-
-        if (dealerTotal > playerTotal) {
-            System.out.println("\nSorry, the house wins.");
-        } else {
-            System.out.println("\nCongrats, you win!");
+        System.out.println(name + " exiting.");
+    }
+}
+class MultiThread {
+    public static void main(String args[]) {
+        new MyThread("One");
+        new MyThread("Two");
+        new MyThread("Three");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Main thread Interrupted");
         }
-        System.out.println("\nGoodbye.");
-
-
+        System.out.println("Main thread exiting.");
     }
 }
